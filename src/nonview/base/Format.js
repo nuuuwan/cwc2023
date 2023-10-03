3456666bae9a07a4542391f7993c6b5dbddf69a1
nuuuwan <nuuuwan@gmail.com>
2023-10-03 19:01:26 +0530

    Removed GroupStatePointsTableView

--- src/nonview/base/Format.js
+++ src/nonview/base/Format.js
@@ -10,7 +10,7 @@ export default class Format {
     return date.toLocaleDateString(undefined, {
       month: "short",
       day: "numeric",
-      weekday: 'short',
+      weekday: "short",
     });
   }
 }
