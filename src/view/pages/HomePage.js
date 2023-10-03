b66a3a5f8c50bc789a6b0fa77d03c261ec3482bf
nuuuwan <nuuuwan@gmail.com>
2023-10-03 16:24:44 +0530

    various

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,13 +1,34 @@
 import { Component } from "react";
 import { STYLE } from "./HomePageStyle";
-import { Box } from "@mui/material";
+import { Box, AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import {MenuIcon} from "@mui/icons-material";
+
 import GroupStageView from "../molecules/GroupStageView";
 import Simulator from "../../nonview/core/Simulator.js";
 import GroupStatePointsTableView from "../molecules/GroupStatePointsTableView";
 import KnockOutStageView from "../molecules/KnockOutStageView";
 export default class HomePage extends Component {
   renderHeader() {
-    return "TODO-Header";
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            #CWC2023
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    )
   }
   renderBody() {
     const simulator = new Simulator();
