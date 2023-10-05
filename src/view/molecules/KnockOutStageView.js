6f5f762d2a79a8a24058b8662b1b0c8a4e28fb66
nuuuwan <nuuuwan@gmail.com>
2023-10-05 10:44:09 +0530

    Screenshot KO stage

--- src/view/molecules/KnockOutStageView.js
+++ src/view/molecules/KnockOutStageView.js
@@ -1,5 +1,7 @@
 import ODIView from "./ODIView.js";
 import { Grid, Box, Typography } from "@mui/material";
+import React from "react";
+import Screenshottable from "./Screenshottable.js";
 
 function renderGroup(entries, odiIdx, odiStateIdx) {
   const onClickODIDummy = function () {};
@@ -31,11 +33,18 @@ export default function KnockOutStageView({
   const final = entries.slice(0, 1);
   const semiFinals = entries.slice(1, 3);
   return (
-    <Box>
-      <Typography variant="h5">Final</Typography>
-      {renderGroup(final, simulator.stats.odiIdx, odiStateIdx, onClickODI)}
-      <Typography variant="h5">Semi-Finals</Typography>
-      {renderGroup(semiFinals, simulator.stats.odiIdx, odiStateIdx, onClickODI)}
-    </Box>
+    <Screenshottable label="knock-out-stage">
+      <Box>
+        <Typography variant="h5">Final</Typography>
+        {renderGroup(final, simulator.stats.odiIdx, odiStateIdx, onClickODI)}
+        <Typography variant="h5">Semi-Finals</Typography>
+        {renderGroup(
+          semiFinals,
+          simulator.stats.odiIdx,
+          odiStateIdx,
+          onClickODI
+        )}
+      </Box>
+    </Screenshottable>
   );
 }
