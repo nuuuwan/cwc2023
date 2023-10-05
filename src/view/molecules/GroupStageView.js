e64ebf63f8002ec523caaf142d59d40ab970c53d
nuuuwan <nuuuwan@gmail.com>
2023-10-05 10:14:01 +0530

    FIXED child key BUG

--- src/view/molecules/GroupStageView.js
+++ src/view/molecules/GroupStageView.js
@@ -3,6 +3,7 @@ import { GROUP_STAGE_ODI_LIST } from "../../nonview/core/GROUP_STAGE_ODI_LIST.js
 import ODIView from "./ODIView.js";
 import { Grid, Box, Typography, Paper } from "@mui/material";
 import ODI from "../../nonview/core/ODI.js";
+import Screenshottable from "../molecules/Screenshottable.js";
 
 const SX_PAPER = { padding: 0.5, margin: 0.5 };
 const SX_GRID = { margin: 0.5, padding: 0.5 };
@@ -15,23 +16,25 @@ function GroupStageWeekView({
   odiStateIdx,
 }) {
   return (
-    <Paper sx={SX_PAPER} elevation={0}>
-      <Typography variant="h6">Week {week}</Typography>
-      <Grid container sx={SX_GRID}>
-        {odiList.map(function (odi) {
-          return (
-            <Grid key={"odi-" + odi.id} item>
-              <ODIView
-                odi={odi}
-                winner={resultIdx[odi.id]}
-                onClickODI={onClickODI}
-                odiState={odiStateIdx[odi.id]}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Paper>
+    <Screenshottable label={`group-state-week-${week}`}>
+      <Paper sx={SX_PAPER} elevation={0}>
+        <Typography variant="h6">Week {week}</Typography>
+        <Grid container sx={SX_GRID}>
+          {odiList.map(function (odi) {
+            return (
+              <Grid key={"odi-" + odi.id} item>
+                <ODIView
+                  odi={odi}
+                  winner={resultIdx[odi.id]}
+                  onClickODI={onClickODI}
+                  odiState={odiStateIdx[odi.id]}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Paper>
+    </Screenshottable>
   );
 }
 
@@ -44,6 +47,7 @@ export default function GroupStageView({ resultIdx, onClickODI, odiStateIdx }) {
         .map(function ([week, odiList]) {
           return (
             <GroupStageWeekView
+              key={"week-" + week}
               week={week}
               odiList={odiList}
               resultIdx={resultIdx}
