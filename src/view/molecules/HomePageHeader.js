5c800932544b7678a13f8e302eb30b402397b18a
nuuuwan <nuuuwan@gmail.com>
2023-10-06 16:21:11 +0530

    Use winner country color

--- src/view/molecules/HomePageHeader.js
+++ src/view/molecules/HomePageHeader.js
@@ -1,10 +1,14 @@
 import { Box, AppBar, Toolbar, Typography } from "@mui/material";
 import AliveView from "./AliveView";
 import { onClickRefresh } from "../Common.js";
+import { TEAM } from "../../nonview/core/Team.js";
+
 export default function HomePageHeader({ bigTable }) {
+  const { teamIDToWinner } = bigTable.stats;
+  const winner = TEAM[Object.keys(teamIDToWinner)[0]];
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ background: winner.color }}>
         <Toolbar>
           <Typography
             variant="h4"
