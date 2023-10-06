8f9d6b0a868d1e2b1730039d878aeb710d8eac6c
nuuuwan <nuuuwan@gmail.com>
2023-10-06 16:22:55 +0530

    In play header

--- src/view/molecules/NextMatchesTableView.js
+++ src/view/molecules/NextMatchesTableView.js
@@ -61,7 +61,7 @@ export default function NextMatchesTableView({ bigTable, odiList }) {
                   </TableCell>
                 );
               })}
-              <TableCell align="center"></TableCell>
+              <TableCell align="center">In Play</TableCell>
             </TableRow>
           </TableHead>
 
