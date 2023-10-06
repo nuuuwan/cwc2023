e1e89d583c5d4c0100af225f06b646db03593939
nuuuwan <nuuuwan@gmail.com>
2023-10-06 16:36:21 +0530

    N_MONTE_CARLO_SIMULATIONS = 25_000

--- src/nonview/constants/STATISTICS.js
+++ src/nonview/constants/STATISTICS.js
@@ -1,5 +1,5 @@
 export const MIN_STATISTICAL_N = 30;
 export const MIN_NO_CHANCE_N = 100;
-export const N_MONTE_CARLO_SIMULATIONS = 10_000;
+export const N_MONTE_CARLO_SIMULATIONS = 25_000;
 export const EPSILON = 0.1 / MIN_NO_CHANCE_N;
 export const P_IS_IN_PLAY = 0.095;
