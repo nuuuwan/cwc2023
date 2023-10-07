c0d578d0c1c4bec95f894a50bdb89d8132f39c6f
nuuuwan <nuuuwan@gmail.com>
2023-10-07 11:34:30 +0530

    Updated odds

--- src/nonview/data/ODI_ID_TO_TEAM_ID_TO_ODDS.js
+++ src/nonview/data/ODI_ID_TO_TEAM_ID_TO_ODDS.js
@@ -1,17 +1,16 @@
 import { TEAM } from "../core/Team";
 export const ODI_ID_TO_TEAM_ID_TO_ODDS = {
   // 01-10
-  "ODI 01":  {[TEAM.ENG.id]: 1.8, [TEAM.NZL.id]: 2.2},  //  OC-2023-10-05
-  "ODI 02":  {[TEAM.PAK.id]: 1.04, [TEAM.NLD.id]: 11.05},  //  OC-2023-10-06
-  "ODI 03":  {[TEAM.AFG.id]: 2.5, [TEAM.BGD.id]: 1.8},  //  OC-2023-10-07
-  "ODI 04":  {[TEAM.ZAF.id]: 1.44, [TEAM.LKA.id]: 3.1},  //  OC-2023-10-07
-  "ODI 05":  {[TEAM.AUS.id]: 2.37, [TEAM.IND.id]: 1.67},  //  OC-2023-10-07
-  "ODI 06":  {[TEAM.NZL.id]: 1.06, [TEAM.NLD.id]: 11},  //  OC-2023-10-07
-  "ODI 07":  {[TEAM.BGD.id]: 4.8, [TEAM.ENG.id]: 1.18},  //  OP-2023-10-07
-  "ODI 08":  {[TEAM.PAK.id]: 1.46, [TEAM.LKA.id]: 2.73},  //  OP-2023-10-07
-  "ODI 09":  {[TEAM.AFG.id]: 5.1, [TEAM.IND.id]: 1.17},  //  OP-2023-10-07
-  "ODI 10":  {[TEAM.AUS.id]: 1.63, [TEAM.ZAF.id]: 2.29},  //  OP-2023-10-07
-  
+  "ODI 01": { [TEAM.ENG.id]: 1.8, [TEAM.NZL.id]: 2.2 }, //  OC-2023-10-05
+  "ODI 02": { [TEAM.PAK.id]: 1.04, [TEAM.NLD.id]: 11.05 }, //  OC-2023-10-06
+  "ODI 03": { [TEAM.AFG.id]: 2.5, [TEAM.BGD.id]: 1.8 }, //  OC-2023-10-07
+  "ODI 04": { [TEAM.ZAF.id]: 1.44, [TEAM.LKA.id]: 3.1 }, //  OC-2023-10-07
+  "ODI 05": { [TEAM.AUS.id]: 2.37, [TEAM.IND.id]: 1.67 }, //  OC-2023-10-07
+  "ODI 06": { [TEAM.NZL.id]: 1.06, [TEAM.NLD.id]: 11 }, //  OC-2023-10-07
+  "ODI 07": { [TEAM.BGD.id]: 4.8, [TEAM.ENG.id]: 1.18 }, //  OP-2023-10-07
+  "ODI 08": { [TEAM.PAK.id]: 1.46, [TEAM.LKA.id]: 2.73 }, //  OP-2023-10-07
+  "ODI 09": { [TEAM.AFG.id]: 5.1, [TEAM.IND.id]: 1.17 }, //  OP-2023-10-07
+  "ODI 10": { [TEAM.AUS.id]: 1.63, [TEAM.ZAF.id]: 2.29 }, //  OP-2023-10-07
 
   // 11-20
 };
