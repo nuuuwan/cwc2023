f589fa49abe4729c53a6153672737f7315c43569
nuuuwan <nuuuwan@gmail.com>
2023-10-07 15:06:43 +0530

    const N_NEXT_MATCHES = 2;

--- src/nonview/statistics/BigTable.js
+++ src/nonview/statistics/BigTable.js
@@ -11,7 +11,7 @@ import ODI from "../core/ODI.js";
 export const PERCENTILES = [
   0.0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1.0,
 ];
-const N_NEXT_MATCHES = 0;
+const N_NEXT_MATCHES = 2;
 
 export default class BigTable {
   constructor(odiStateIdx) {
