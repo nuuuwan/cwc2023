40cca1da7151bd73f477c8346a92439080e3f0fc
nuuuwan <nuuuwan@gmail.com>
2023-10-08 14:40:36 +0530

    export const PACK_RATIO = Math.pow(2, 0.25);

--- src/nonview/constants/STATISTICS.js
+++ src/nonview/constants/STATISTICS.js
@@ -3,5 +3,5 @@ export const MIN_NO_CHANCE_N = 100;
 export const N_MONTE_CARLO_SIMULATIONS = 10_000;
 export const EPSILON = 1.0 / 1_000_000_000;
 export const P_IS_IN_PLAY = 0.095;
-export const PACK_RATIO = Math.pow(2, 0.5);
+export const PACK_RATIO = Math.pow(2, 0.25);
 export const MIN_P_FOR_PACK = 0.01;
