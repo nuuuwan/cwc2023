f85efed2b90eea744c3960d289832535dc7e6b1b
nuuuwan <nuuuwan@gmail.com>
2023-10-08 10:14:21 +0530

    various

--- workflows/update_data.py
+++ workflows/update_data.py
@@ -1,11 +1,11 @@
-from workflows import (update_odi_id_to_winner, update_team_id_to_p_winner)
+from workflows import update_odi_id_to_winner, update_team_id_to_p_winner
 
 # python workflows/update_data.py ; jlic "Updated Data";
+
 
 def main():
     update_team_id_to_p_winner.main()
     update_odi_id_to_winner.main()
-
 
 
 if __name__ == '__main__':
