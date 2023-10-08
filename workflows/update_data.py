b299a0fc014d3d8102944db56d55d44df69067a4
nuuuwan <nuuuwan@gmail.com>
2023-10-08 09:52:08 +0530

    Added update_version

--- workflows/update_data.py
+++ workflows/update_data.py
@@ -1,9 +1,11 @@
-from workflows import update_odi_id_to_winner, update_team_id_to_p_winner
+from workflows import (update_odi_id_to_winner, update_team_id_to_p_winner,
+                       update_version)
 
 
 def main():
     update_team_id_to_p_winner.main()
     update_odi_id_to_winner.main()
+    update_version.main()
 
 
 if __name__ == '__main__':
