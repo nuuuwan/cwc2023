c5a6119c3f70e490355273bf13c15c1cab31848e
nuuuwan <nuuuwan@gmail.com>
2023-10-09 18:01:19 +0530

    Disabled Auto Scroll

--- src/view/molecules/GroupStageWeekView.js
+++ src/view/molecules/GroupStageWeekView.js
@@ -3,10 +3,8 @@ import ScreenShot from "./ScreenShot.js";
 import { EMOJI } from "../../nonview/constants/EMOJI.js";
 import ODIGroupView from "./ODIGroupView.js";
 import React from "react";
-import DateX from "../../nonview/base/DateX.js";
 
 const SX_PAPER = { padding: 1, margin: 0.5 };
-const T_SCROLL_DELAY = 200;
 function getTweetBody(week, odiList) {
   let lines = [`Week ${week} - Predictions`];
   for (let odi of odiList.slice()) {
@@ -25,37 +23,22 @@ export default function GroupStageWeekView({
   odiStateIdx,
   setSnackbarMessage,
 }) {
-  let refThis = React.createRef();
-
-  setTimeout(function () {
-    const isCurrentWeek = parseInt(week) === DateX.now().week;
-    if (refThis && isCurrentWeek) {
-      refThis.scrollIntoView({ behavior: "smooth" });
-    }
-  }, T_SCROLL_DELAY);
-
   return (
-    <div
-      ref={(ref) => {
-        refThis = ref;
-      }}
+    <ScreenShot
+      label={`group-state-week-${week}`}
+      tweetBody={getTweetBody(week, odiList)}
+      setSnackbarMessage={setSnackbarMessage}
     >
-      <ScreenShot
-        label={`group-state-week-${week}`}
-        tweetBody={getTweetBody(week, odiList)}
-        setSnackbarMessage={setSnackbarMessage}
-      >
-        <Box sx={SX_PAPER}>
-          <Typography variant="h6">Week {week}</Typography>
-          <ODIGroupView
-            odiList={odiList}
-            simulator={simulator}
-            onClickODI={onClickODI}
-            odiStateIdx={odiStateIdx}
-            showWinner={true}
-          />
-        </Box>
-      </ScreenShot>
-    </div>
+      <Box sx={SX_PAPER}>
+        <Typography variant="h6">Week {week}</Typography>
+        <ODIGroupView
+          odiList={odiList}
+          simulator={simulator}
+          onClickODI={onClickODI}
+          odiStateIdx={odiStateIdx}
+          showWinner={true}
+        />
+      </Box>
+    </ScreenShot>
   );
 }
