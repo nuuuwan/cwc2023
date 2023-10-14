732d663b675a05c3020ec810bc69a0906efa5399
nuuuwan <nuuuwan@gmail.com>
2023-10-14 09:45:38 -0700

    Updated data without JS Lint

--- workflows/update_data.py
+++ workflows/update_data.py
@@ -1,4 +1,4 @@
-from workflows import (update_odi_id_to_winner,
+from workflows import (update_odi_id_to_winner, update_points_table,
                        update_team1_id_to_team2_id_to_odds_pair,
                        update_team_id_to_p_winner, update_version)
 
@@ -7,6 +7,8 @@ def main():
     update_team_id_to_p_winner.main()
     update_odi_id_to_winner.main()
     update_team1_id_to_team2_id_to_odds_pair.main()
+    update_points_table.main()
+
     update_version.main()
 
 
