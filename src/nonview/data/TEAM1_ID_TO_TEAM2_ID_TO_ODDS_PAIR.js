a0d1d2daf118de35e9675cd7cbc6f7fd14a1271f
nuuuwan <nuuuwan@gmail.com>
2023-10-30 14:54:52 +0530

    lint only

--- src/nonview/data/TEAM1_ID_TO_TEAM2_ID_TO_ODDS_PAIR.js
+++ src/nonview/data/TEAM1_ID_TO_TEAM2_ID_TO_ODDS_PAIR.js
@@ -18,7 +18,7 @@ export const TEAM1_ID_TO_TEAM2_ID_TO_ODDS_PAIR = {
     [TEAM.LKA.id]: [1.19, 4.73],
   },
   [TEAM.NZL.id]: {
-    [TEAM.PAK.id]: [1.73, 2.10],
+    [TEAM.PAK.id]: [1.73, 2.1],
   },
   [TEAM.AUS.id]: {
     [TEAM.ENG.id]: [1.54, 2.45],
