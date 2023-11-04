a7332997e45d01e9cd145efe03c5a613fb17d768
Nuwan I. Senaratna <nuuuwan@gmail.com>
2023-11-04 23:25:25 +0530

    Update STATISTICS.js

--- src/nonview/constants/STATISTICS.js
+++ src/nonview/constants/STATISTICS.js
@@ -1,6 +1,6 @@
 export const MIN_STATISTICAL_N = 30;
 export const MIN_NO_CHANCE_N = 100;
-export const N_MONTE_CARLO_SIMULATIONS = 10_000;
+export const N_MONTE_CARLO_SIMULATIONS = 100_000;
 export const EPSILON = 1 / N_MONTE_CARLO_SIMULATIONS;
 export const P_IS_IN_PLAY = 0.095;
 export const PACK_RATIO = Math.pow(2, 0.25);
