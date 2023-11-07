9cae2080c3f37a0f1a21eb146b53f6d61c9ac3a9
nuuuwan <nuuuwan@gmail.com>
2023-11-07 19:50:39 +0530

    Fixed NRR Bug

--- src/nonview/statistics/GroupStatePointsTable.js
+++ src/nonview/statistics/GroupStatePointsTable.js
@@ -5,7 +5,10 @@ export default class GroupStagePointsTable {
   constructor(resultIdx) {
     this.resultIdx = resultIdx;
   }
-
+  static getNRRRandomRemainder(nPlayed) {
+    const K_NRR = 1;
+    return (9-nPlayed) * (Math.random() - 0.5) * 2* K_NRR;
+  }
   getTeamToWins() {
     const teamToWins = Object.values(this.resultIdx).reduce(function (
       idx,
@@ -25,9 +28,12 @@ export default class GroupStagePointsTable {
         const ptrA = TEAM_ID_TO_POINTS_TABLE_ROW[idA];
         const ptrB = TEAM_ID_TO_POINTS_TABLE_ROW[idB];
 
-        const nrrA = ptrA.netRunRate;
-        const nrrB = ptrB.netRunRate;
-        return nrrA - nrrB;
+        const nrrA = ptrA.netRunRate + GroupStagePointsTable.getNRRRandomRemainder(ptrA.nPlayed);
+        const nrrB = ptrB.netRunRate +  GroupStagePointsTable.getNRRRandomRemainder(ptrB.nPlayed);
+        if (nrrA !== nrrB) {
+          return nrrB - nrrA;
+        }
+        return Math.random() - 0.5;
       })
     );
     return sortedTeamToWins;
